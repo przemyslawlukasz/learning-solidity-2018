4b53716a78748674f5243465ff03f07539ce6c1f
Przemyslaw Kislo <przemyslawlukasz@gmail.com>
2018-09-12 01:54:52 +0200

    encodePacked used to prepare keccak256 argument

--- 03_basic_random/03_basic_random.sol
+++ 03_basic_random/03_basic_random.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.24;
 
 contract BasicRandom {
-    function generateNumber() view returns(uint8) {
-        return uint8(keccak256(block.number + uint256(msg.sender))) % 100;
+    function generateNumber() view public returns(uint8) {
+        return uint8(keccak256(abi.encodePacked(now,uint256(msg.sender)))) % 100;
     }
 }
