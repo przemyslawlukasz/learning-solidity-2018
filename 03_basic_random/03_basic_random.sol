5b8324bf3cc77799dd7ddd61a36c1374fe9fd155
Przemyslaw Kislo <przemyslawlukasz@gmail.com>
2018-09-12 10:34:36 +0200

    add ability to change bit and number of participants

--- 03_basic_random/03_basic_random.sol
+++ 03_basic_random/03_basic_random.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.4.24;
 
 contract RandomGenerator {
-    function generateNumber() view public returns(uint);
+    function generateNumber(uint) view public returns(uint);
 }
 
 contract BasicRandom is RandomGenerator {
-    function generateNumber() view public returns(uint) {
-        return uint8(keccak256(abi.encodePacked(now,uint256(msg.sender)))) % 100;
+    function generateNumber(uint _range) view public returns(uint) {
+        return uint8(keccak256(abi.encodePacked(now,uint256(msg.sender)))) % _range;
     }
 }
--- 04_lottery_10_users/04_lottery_10_users.sol
+++ 04_lottery_10_users/04_lottery_10_users.sol
@@ -1,31 +1,38 @@
 pragma solidity ^0.4.24;
 import "../03_basic_random/03_basic_random.sol";
 
-contract Lottery4TenUsers {
+contract Lottery4NUsers {
     
     event LotteryNewParticipantJoin(address indexed);
     event LotteryWinner(address indexed);
     
+    modifier onlyBetweenLotteries {
+        require(participants.length == 0, "You cannot change parameters during lottery");
+        _;
+    }
+    
     RandomGenerator randomGenerator;
     
-    uint constant participantsLimit = 5;
-    uint constant BID = 100 finney;
+    uint participantsLimit;
+    uint bid;
     mapping (address => bool) participantFlag;
     address[] participants;
     
-    constructor(address _randomGenerator) public {
+    constructor(address _randomGenerator, uint _participantsLimit, uint _bid) public {
         randomGenerator = RandomGenerator(_randomGenerator);
+        participantsLimit = _participantsLimit;
+        bid = _bid;
     }
     
     function join() external payable {
         require(participantFlag[msg.sender] == false, "You've joined already");
-        require(msg.value == BID, "You need to pay 0.1 ether");
+        require(msg.value == bid, "You need to pay proper BID value");
         participantFlag[msg.sender] = true;
         participants.push(msg.sender);
         emit LotteryNewParticipantJoin(msg.sender);
         if(participants.length == participantsLimit) {
-            address winner = participants[randomGenerator.generateNumber() % participantsLimit];
-            winner.transfer(participantsLimit * BID);
+            address winner = participants[randomGenerator.generateNumber(participantsLimit)];
+            winner.transfer(participantsLimit * bid);
             emit LotteryWinner(winner);
             clearParticipants();
         }
@@ -38,8 +45,16 @@ contract Lottery4TenUsers {
         participants.length = 0;
     }
     
-    function setRandomGeneratorContractAddress(address _randomGenerator) external {
-        require(participants.length == 0, "You cannot change randomGenerator during lottery");
+    function setRandomGeneratorContractAddress(address _randomGenerator) external onlyBetweenLotteries {
         randomGenerator = RandomGenerator(_randomGenerator);
     }
+    
+    function setBid(uint _bid) external onlyBetweenLotteries {
+        bid = _bid;
+    }
+    
+    function setNumberOfParticipants(uint _participantsLimit) external onlyBetweenLotteries {
+        participantsLimit = _participantsLimit;
+    }
+    
 }
